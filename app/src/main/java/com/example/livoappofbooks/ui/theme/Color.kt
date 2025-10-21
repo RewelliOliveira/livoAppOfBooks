10466caa73433241d04ad8705c5f07e2d650bf1a
RewelliOliveira <rewelli@alu.ufc.br>
2025-10-21 19:52:49 -0300

    config: themas personalizados

--- app/src/main/java/com/example/livoappofbooks/ui/theme/Color.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/theme/Color.kt
@@ -2,14 +2,6 @@ package com.example.livoappofbooks.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val PrincipalColor = Color(0xFF003D3A)
 val DarkColor = Color(0xFF001327)
 val PositiveActions = Color(0xFF2BA80C)
