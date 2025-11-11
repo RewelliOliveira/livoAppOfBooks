8d483d910090cddeeaa3aa4daf1f1d07cb2d12b5
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-11-11 01:29:29 -0300

    feat: adição de icones

--- app/src/main/java/com/example/livoappofbooks/ui/icons/CustomIcons.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/icons/CustomIcons.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
 
 val Arrow_back_ios_new: ImageVector
     get() {
@@ -33,5 +35,278 @@ val Arrow_back_ios_new: ImageVector
         return _Arrow_back_ios_new!!
     }
 
+val BuildingLibrary: ImageVector
+    get() {
+        if (_BuildingLibrary != null) return _BuildingLibrary!!
+
+        _BuildingLibrary = ImageVector.Builder(
+            name = "BuildingLibrary",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round
+            ) {
+                moveTo(12f, 21f)
+                verticalLineTo(12.75f)
+                moveTo(15.75f, 21f)
+                verticalLineTo(12.75f)
+                moveTo(8.25f, 21f)
+                verticalLineTo(12.75f)
+                moveTo(3f, 9f)
+                lineTo(12f, 3f)
+                lineTo(21f, 9f)
+                moveTo(19.5f, 21f)
+                verticalLineTo(10.3325f)
+                curveTo(17.0563f, 9.94906f, 14.5514f, 9.75f, 12f, 9.75f)
+                curveTo(9.44861f, 9.75f, 6.94372f, 9.94906f, 4.5f, 10.3325f)
+                verticalLineTo(21f)
+                moveTo(3f, 21f)
+                horizontalLineTo(21f)
+                moveTo(12f, 6.75f)
+                horizontalLineTo(12.0075f)
+                verticalLineTo(6.7575f)
+                horizontalLineTo(12f)
+                verticalLineTo(6.75f)
+                close()
+            }
+        }.build()
+
+        return _BuildingLibrary!!
+    }
+
+
+val BookOpen: ImageVector
+    get() {
+        if (_BookOpen != null) return _BookOpen!!
+
+        _BookOpen = ImageVector.Builder(
+            name = "BookOpen",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round
+            ) {
+                moveTo(12f, 6.04168f)
+                curveTo(10.4077f, 4.61656f, 8.30506f, 3.75f, 6f, 3.75f)
+                curveTo(4.94809f, 3.75f, 3.93834f, 3.93046f, 3f, 4.26212f)
+                verticalLineTo(18.5121f)
+                curveTo(3.93834f, 18.1805f, 4.94809f, 18f, 6f, 18f)
+                curveTo(8.30506f, 18f, 10.4077f, 18.8666f, 12f, 20.2917f)
+                moveTo(12f, 6.04168f)
+                curveTo(13.5923f, 4.61656f, 15.6949f, 3.75f, 18f, 3.75f)
+                curveTo(19.0519f, 3.75f, 20.0617f, 3.93046f, 21f, 4.26212f)
+                verticalLineTo(18.5121f)
+                curveTo(20.0617f, 18.1805f, 19.0519f, 18f, 18f, 18f)
+                curveTo(15.6949f, 18f, 13.5923f, 18.8666f, 12f, 20.2917f)
+                moveTo(12f, 6.04168f)
+                verticalLineTo(20.2917f)
+            }
+        }.build()
+
+        return _BookOpen!!
+    }
+
+val CalendarDays: ImageVector
+    get() {
+        if (_CalendarDays != null) return _CalendarDays!!
+
+        _CalendarDays = ImageVector.Builder(
+            name = "CalendarDays",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round
+            ) {
+                moveTo(6.75f, 3f)
+                verticalLineTo(5.25f)
+                moveTo(17.25f, 3f)
+                verticalLineTo(5.25f)
+                moveTo(3f, 18.75f)
+                verticalLineTo(7.5f)
+                curveTo(3f, 6.25736f, 4.00736f, 5.25f, 5.25f, 5.25f)
+                horizontalLineTo(18.75f)
+                curveTo(19.9926f, 5.25f, 21f, 6.25736f, 21f, 7.5f)
+                verticalLineTo(18.75f)
+                moveTo(3f, 18.75f)
+                curveTo(3f, 19.9926f, 4.00736f, 21f, 5.25f, 21f)
+                horizontalLineTo(18.75f)
+                curveTo(19.9926f, 21f, 21f, 19.9926f, 21f, 18.75f)
+                moveTo(3f, 18.75f)
+                verticalLineTo(11.25f)
+                curveTo(3f, 10.0074f, 4.00736f, 9f, 5.25f, 9f)
+                horizontalLineTo(18.75f)
+                curveTo(19.9926f, 9f, 21f, 10.0074f, 21f, 11.25f)
+                verticalLineTo(18.75f)
+                moveTo(12f, 12.75f)
+                horizontalLineTo(12.0075f)
+                verticalLineTo(12.7575f)
+                horizontalLineTo(12f)
+                verticalLineTo(12.75f)
+                close()
+                moveTo(12f, 15f)
+                horizontalLineTo(12.0075f)
+                verticalLineTo(15.0075f)
+                horizontalLineTo(12f)
+                verticalLineTo(15f)
+                close()
+                moveTo(12f, 17.25f)
+                horizontalLineTo(12.0075f)
+                verticalLineTo(17.2575f)
+                horizontalLineTo(12f)
+                verticalLineTo(17.25f)
+                close()
+                moveTo(9.75f, 15f)
+                horizontalLineTo(9.7575f)
+                verticalLineTo(15.0075f)
+                horizontalLineTo(9.75f)
+                verticalLineTo(15f)
+                close()
+                moveTo(9.75f, 17.25f)
+                horizontalLineTo(9.7575f)
+                verticalLineTo(17.2575f)
+                horizontalLineTo(9.75f)
+                verticalLineTo(17.25f)
+                close()
+                moveTo(7.5f, 15f)
+                horizontalLineTo(7.5075f)
+                verticalLineTo(15.0075f)
+                horizontalLineTo(7.5f)
+                verticalLineTo(15f)
+                close()
+                moveTo(7.5f, 17.25f)
+                horizontalLineTo(7.5075f)
+                verticalLineTo(17.2575f)
+                horizontalLineTo(7.5f)
+                verticalLineTo(17.25f)
+                close()
+                moveTo(14.25f, 12.75f)
+                horizontalLineTo(14.2575f)
+                verticalLineTo(12.7575f)
+                horizontalLineTo(14.25f)
+                verticalLineTo(12.75f)
+                close()
+                moveTo(14.25f, 15f)
+                horizontalLineTo(14.2575f)
+                verticalLineTo(15.0075f)
+                horizontalLineTo(14.25f)
+                verticalLineTo(15f)
+                close()
+                moveTo(14.25f, 17.25f)
+                horizontalLineTo(14.2575f)
+                verticalLineTo(17.2575f)
+                horizontalLineTo(14.25f)
+                verticalLineTo(17.25f)
+                close()
+                moveTo(16.5f, 12.75f)
+                horizontalLineTo(16.5075f)
+                verticalLineTo(12.7575f)
+                horizontalLineTo(16.5f)
+                verticalLineTo(12.75f)
+                close()
+                moveTo(16.5f, 15f)
+                horizontalLineTo(16.5075f)
+                verticalLineTo(15.0075f)
+                horizontalLineTo(16.5f)
+                verticalLineTo(15f)
+                close()
+            }
+        }.build()
+
+        return _CalendarDays!!
+    }
+
+val Language: ImageVector
+    get() {
+        if (_Language != null) return _Language!!
+
+        _Language = ImageVector.Builder(
+            name = "Language",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round
+            ) {
+                moveTo(10.5f, 21f)
+                lineTo(15.75f, 9.75f)
+                lineTo(21f, 21f)
+                moveTo(12f, 18f)
+                horizontalLineTo(19.5f)
+                moveTo(3f, 5.62136f)
+                curveTo(4.96557f, 5.37626f, 6.96804f, 5.25f, 9f, 5.25f)
+                moveTo(9f, 5.25f)
+                curveTo(10.1208f, 5.25f, 11.2326f, 5.28841f, 12.3343f, 5.364f)
+                moveTo(9f, 5.25f)
+                verticalLineTo(3f)
+                moveTo(12.3343f, 5.364f)
+                curveTo(11.1763f, 10.6578f, 7.68868f, 15.0801f, 3f, 17.5023f)
+                moveTo(12.3343f, 5.364f)
+                curveTo(13.2298f, 5.42545f, 14.1186f, 5.51146f, 15f, 5.62136f)
+                moveTo(10.4113f, 14.1162f)
+                curveTo(8.78554f, 12.4619f, 7.47704f, 10.4949f, 6.58432f, 8.31366f)
+            }
+        }.build()
+
+        return _Language!!
+    }
+
+val MarcaPagina: ImageVector
+    get() {
+        if (_MarcaPagina != null) return _MarcaPagina!!
+        _MarcaPagina = ImageVector.Builder(
+            name = "MarcaPagina",
+            defaultWidth = 45.dp,
+            defaultHeight = 78.dp,
+            viewportWidth = 45f,
+            viewportHeight = 78f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFFFDFBED))
+            ) {
+                moveTo(0f, 22.4896f)
+                verticalLineTo(70.5443f)
+                curveTo(0f, 74.3074f, 1.54875f, 76.2148f, 3.52174f, 76.7998f)
+                curveTo(10.6555f, 78.9148f, 14.5593f, 65.1601f, 22f, 65.1601f)
+                curveTo(29.5712f, 65.1601f, 34.0831f, 78.4818f, 41.4052f, 76.5557f)
+                curveTo(43.4305f, 76.023f, 45f, 74.2222f, 45f, 70.5443f)
+                verticalLineTo(22.4896f)
+                curveTo(45f, 10.0632f, 34.9264f, 0f, 22.5f, 0f)
+                curveTo(10.0736f, 0f, 0f, 10.0632f, 0f, 22.4896f)
+                close()
+            }
+        }.build()
+        return _MarcaPagina!!
+    }
+
+private var _MarcaPagina: ImageVector? = null
+
+
+private var _Language: ImageVector? = null
+private var _CalendarDays: ImageVector? = null
+private var _BookOpen: ImageVector? = null
+private var _BuildingLibrary: ImageVector? = null
 private var _Arrow_back_ios_new: ImageVector? = null
 
