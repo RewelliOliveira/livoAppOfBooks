892dfc1746cab7e186d3957c598c7be686c717eb
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-11-17 14:27:52 -0300

    refac: alterando starRating para ser opcionalmente mutável via toque

--- app/src/main/java/com/example/livoappofbooks/ui/components/StarRating.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/StarRating.kt
@@ -1,6 +1,11 @@
 package com.example.livoappofbooks.ui.components
 
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -10,6 +15,11 @@ import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +33,8 @@ fun StarRating(
     rating: Double,
     maxStars: Int = 5,
     starSize: Int = 24,
-    starColor: Color = PrincipalColor
+    starColor: Color = PrincipalColor,
+    onRatingChange: ((Double) -> Unit)? = null //opcional, será passado quando o modal for clicavel
 ) {
     Row {
         val fullStars = floor(rating).toInt()
@@ -32,7 +43,7 @@ fun StarRating(
         for (i in 1..maxStars) {
             val icon = when {
                 i <= fullStars -> Icons.Rounded.Star
-                i == fullStars + 1 && hasHalfStar -> Icons.AutoMirrored.Rounded.StarHalf
+                i == fullStars + 1 && hasHalfStar -> Icons.Rounded.StarHalf
                 else -> Icons.Outlined.StarOutline
             }
 
@@ -41,6 +52,11 @@ fun StarRating(
                 contentDescription = null,
                 tint = starColor,
                 modifier = Modifier.size(starSize.dp)
+                    .then(
+                        if (onRatingChange != null)
+                            Modifier.clickable() { onRatingChange(i.toDouble()) }
+                        else Modifier
+                    )
             )
         }
     }
@@ -59,5 +75,23 @@ fun StarRatingPreview() {
 fun StarRatingEmptyPreview() {
     Row(Modifier.padding(16.dp)) {
         StarRating(rating = 2.1)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StarRatingClickable() {
+    var rating by remember { mutableStateOf(0.0) }
+    Row(Modifier.padding(top = 128.dp)) {
+        StarRating(
+            starSize = 56,
+            rating = rating.toDouble(),
+            onRatingChange = { newValue ->
+                rating = if (rating == newValue - 0.5)
+                    newValue
+                else
+                    newValue - 0.5
+            }
+        )
     }
 }
