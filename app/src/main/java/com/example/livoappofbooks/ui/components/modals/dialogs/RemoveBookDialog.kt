fdc856bbb7412e4c9872dda5b1e06a134b7dae59
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-11-17 19:20:38 -0300

    infra: adiconando xml de trash_bin

--- app/src/main/java/com/example/livoappofbooks/ui/components/modals/dialogs/RemoveBookDialog.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/modals/dialogs/RemoveBookDialog.kt
@@ -1,15 +1,11 @@
 package com.example.livoappofbooks.ui.components.modals.dialogs
 
-import android.R.attr.padding
-import androidx.compose.foundation.background
+import com.example.livoappofbooks.R
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Book
@@ -17,22 +13,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.livoappofbooks.ui.components.OutlinedIconLabelButton
-import com.example.livoappofbooks.ui.components.StarRating
-import com.example.livoappofbooks.ui.components.modals.ModalHeader
-import com.example.livoappofbooks.ui.icons.MarcaPagina
-import com.example.livoappofbooks.ui.theme.ThemeProvider
 
 @Composable
 fun RemoveBookDialog(
@@ -45,7 +34,7 @@ fun RemoveBookDialog(
     ) {
         Surface(
             shape = RoundedCornerShape(16.dp),
-            tonalElevation = 4.dp 
+            tonalElevation = 4.dp
         ) {
             Column(
                 modifier = Modifier
@@ -73,13 +62,13 @@ fun RemoveBookDialog(
                     OutlinedIconLabelButton(
                         text = "Cancelar",
                         icon = Icons.Default.Book,
-                        color = Color.Green,
+                        color = MaterialTheme.colorScheme.secondary,
                         onClick = onClick
                     )
                     OutlinedIconLabelButton(
                         text = "Remover",
-                        icon = Icons.Default.Book,
-                        color = Color.Red,
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_trash_bin),
+                        color = MaterialTheme.colorScheme.error,
                         onClick = onClick
                     )
                 }
