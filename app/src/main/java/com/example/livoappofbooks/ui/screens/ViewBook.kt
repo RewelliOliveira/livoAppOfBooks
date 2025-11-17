a3be3aef2371ed8f95480e29f62159de923b563f
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-11-17 19:17:23 -0300

    feat: limitação de caracteres para sinopse e editora

--- app/src/main/java/com/example/livoappofbooks/ui/screens/ViewBook.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/screens/ViewBook.kt
@@ -2,7 +2,9 @@ package com.example.livoappofbooks.ui.screens
 
 import com.example.livoappofbooks.ui.icons.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 
@@ -10,6 +12,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.*
@@ -19,6 +23,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -28,11 +33,10 @@ import com.example.livoappofbooks.ui.components.StarRating
 import com.example.livoappofbooks.ui.components.InfoItem
 import com.example.livoappofbooks.ui.components.PrimaryButton
 import com.example.livoappofbooks.ui.components.Status
+import com.example.livoappofbooks.ui.theme.AlertColor
 import com.example.livoappofbooks.ui.theme.AppTypography
 import com.example.livoappofbooks.ui.theme.FundoClaro
-import com.example.livoappofbooks.ui.theme.PrincipalColor
-
-@Composable
+import com.example.livoappofbooks.ui.theme.PrincipalColor@Composable
 fun ViewBook(
     title: String,
     author: String,
@@ -45,153 +49,206 @@ fun ViewBook(
     shelf: String,
     onBackClick: () -> Unit
 ) {
+
+    val isExpanded = remember { mutableStateOf(false) }
+    val previewLimit = 150
+    val shouldTruncate = sinopse.length > previewLimit
+
+    val displayedSinopse =
+        if (isExpanded.value || !shouldTruncate) sinopse
+        else sinopse.take(previewLimit) + "..."
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(FundoClaro)
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(350.dp),
-            contentAlignment = Alignment.TopCenter
-        ) {
-            AsyncImage(
-                model = imageUrl,
-                placeholder = painterResource(id = R.drawable.livro_teste),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(320.dp)
-                    .blur(10.dp)
-                    .shadow(20.dp)
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(320.dp)
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, FundoClaro.copy(alpha = 1f)),
-                            startY = 250f
-                        )
-                    )
-            )
-
-            AsyncImage(
-                model = imageUrl,
-                placeholder = painterResource(id = R.drawable.livro_teste),
-                contentDescription = title,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .height(260.dp)
-                    .width(170.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .shadow(16.dp, RoundedCornerShape(8.dp))
-                    .align(Alignment.BottomCenter)
-            )
-
-            IconButton(
-                onClick = onBackClick,
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .padding(16.dp)
-                    .size(36.dp)
-                    .background(Color.White, CircleShape)
-            ) {
-                Icon(
-                    imageVector = Arrow_back_ios_new,
-                    contentDescription = "Voltar",
-                    tint = Color.Black
-                )
-            }
-        }
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
-                .padding(horizontal = 24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(Modifier.height(385.dp))
 
-            Row(
-                verticalAlignment = Alignment.Bottom
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(350.dp),
+                contentAlignment = Alignment.TopCenter
             ) {
-                Column(
-                    modifier = Modifier.weight(1f),
-                    horizontalAlignment = Alignment.Start
+
+                AsyncImage(
+                    model = imageUrl,
+                    placeholder = painterResource(id = R.drawable.livro_teste),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(320.dp)
+                        .blur(10.dp)
+                        .shadow(20.dp)
+                )
+
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(320.dp)
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(Color.Transparent, FundoClaro.copy(alpha = 1f)),
+                                startY = 250f
+                            )
+                        )
+                )
+
+                AsyncImage(
+                    model = imageUrl,
+                    placeholder = painterResource(id = R.drawable.livro_teste),
+                    contentDescription = title,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(260.dp)
+                        .width(170.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .shadow(16.dp, RoundedCornerShape(8.dp))
+                        .align(Alignment.BottomCenter)
+                )
+
+            }
+
+            Column(
+                modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Spacer(Modifier.height(16.dp))
+
+                Row(verticalAlignment = Alignment.Bottom) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = title,
+                            style = AppTypography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                            color = PrincipalColor
+                        )
+                        Spacer(Modifier.height(8.dp))
+                        Text(
+                            text = author,
+                            style = AppTypography.bodyMedium,
+                            color = Color.DarkGray
+                        )
+                    }
+                    StarRating(rating = rate)
+                }
+
+                Spacer(Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text(
-                        text = title,
-                        style = AppTypography.titleLarge.copy(fontWeight = FontWeight.Bold),
-                        color = PrincipalColor
+                    InfoItem(icon = CalendarDays, text = publishYear)
+                    InfoItem(icon = BuildingLibrary, text = publisher.take(12))
+                    InfoItem(icon = BookOpen, text = "$pageCount págs")
+                }
+
+                Spacer(Modifier.height(16.dp))
+                Divider(color = Color.LightGray)
+                Spacer(Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    PrimaryButton(
+                        modifier = Modifier.weight(1f),
+                        text = shelf,
+                        onClick = {},
+                        icon = Bookshelf
                     )
-                    Spacer(Modifier.height(8.dp))
+                    Spacer(Modifier.width(16.dp))
+                    Status(status = BookStatus.LENDO, onClick = {})
+                }
+
+                Spacer(Modifier.height(16.dp))
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Sinopse",
+                            style = AppTypography.titleMedium.copy(
+                                color = Color.Black,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                        )
+
+                        Text(
+                            text = displayedSinopse,
+                            style = AppTypography.bodyMedium.copy(color = Color.DarkGray),
+                            textAlign = TextAlign.Justify,
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+
+                        if (shouldTruncate) {
+                            Text(
+                                text = if (isExpanded.value) "Ver menos" else "Ver mais",
+                                style = AppTypography.bodyMedium.copy(
+                                    color = PrincipalColor,
+                                    fontWeight = FontWeight.SemiBold
+                                ),
+                                modifier = Modifier
+                                    .padding(top = 4.dp)
+                                    .clickable { isExpanded.value = !isExpanded.value }
+                            )
+                        }
+                    }
+                    Spacer(Modifier.height(64.dp))
                     Text(
-                        text = author,
-                        style = AppTypography.bodyMedium,
-                        color = Color.DarkGray
+                        text = "Remover livro",
+                        style = AppTypography.titleMedium.copy(
+                            color = AlertColor,
+                            fontWeight = FontWeight.SemiBold,
+                            textDecoration = TextDecoration.Underline
+                        ),
+                        modifier = Modifier
+                            .padding(bottom = 30.dp)
+                            .clickable(onClick = {})
                     )
                 }
-                StarRating(rating = rate)
             }
-            Spacer(Modifier.height(16.dp))
+        }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                InfoItem(icon = CalendarDays, text = publishYear)
-                InfoItem(icon = BuildingLibrary, text = publisher)
-                InfoItem(icon = BookOpen, text = "$pageCount págs")
-
-            }
-
-            Spacer(Modifier.height(16.dp))
-            Divider(color = Color.LightGray)
-            Spacer(Modifier.height(8.dp))
-
-            Row(modifier = Modifier
-                .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                PrimaryButton(
-                    modifier = Modifier.weight(1F),
-                    text = shelf,
-                    onClick = {},
-                    icon = Bookshelf
-                )
-                Spacer(Modifier.width(16.dp))
-                Status(status = BookStatus.LENDO, onClick = {})
-            }
-
-            Spacer(Modifier.height(16.dp))
-
-            Text(
-                text = "Sinopse",
-                style = AppTypography.titleMedium.copy(
-                    color = Color.Black,
-                    fontWeight = FontWeight.SemiBold
-                ),
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Text(
-                text = sinopse,
-                style = AppTypography.bodyMedium.copy(color = Color.DarkGray),
-                textAlign = TextAlign.Justify,
-                modifier = Modifier.padding(top = 8.dp)
+        IconButton(
+            onClick = onBackClick,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(20.dp, 40.dp)
+                .size(16.dp)
+                .background(Color.White, CircleShape)
+        ) {
+            Icon(
+                imageVector = Arrow_back_ios_new,
+                contentDescription = "Voltar",
+                tint = Color.Black,
             )
         }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -201,7 +258,7 @@ fun ViewBookPreview() {
         author = "Samira Sales",
         rate = 3.7,
         sinopse = "Em um mundo onde prestam atenção em cada detalhe do teste do livro, eu mudei o nome que estava antes para ficar mais coerente com a capa e ninguem ficar fazendo zuada no meu pé do ouvido",
-        imageUrl = "url_qualquer",
+        imageUrl = "https://br.pinterest.com/pin/19492210989423958/",
         publishYear = "2025",
         publisher = "Bila-Bilu",
         shelf = "Romances",
