3a3deb3fe1f04a93324f318d5081c75baa5f86a4
RewelliOliveira <rewelli@alu.ufc.br>
2025-11-19 14:16:13 -0300

    feat(SmallOutlinedInput): criacao de inputs personalizaveis

---
+++ app/src/main/java/com/example/livoappofbooks/ui/components/SmallOutlinedTextField.kt
@@ -0,0 +1,29 @@
+package com.example.livoappofbooks.ui.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+@Composable
+fun SmallInput(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label, fontSize = 14.sp) },
+        modifier = modifier.height(43.dp),
+        shape = RoundedCornerShape(12.dp),
+        singleLine = true,
+        textStyle = LocalTextStyle.current.copy(fontSize = 14.sp)
+    )
+}
