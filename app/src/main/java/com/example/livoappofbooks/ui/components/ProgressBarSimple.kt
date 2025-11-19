aa9dfa2d233a4fc775ad7713f7bd2745fdd88796
RewelliOliveira <rewelli@alu.ufc.br>
2025-11-19 12:53:03 -0300

    feat(ProgressBarSimple): criacao da barra de progresso simplificada

---
+++ app/src/main/java/com/example/livoappofbooks/ui/components/ProgressBarSimple.kt
@@ -0,0 +1,58 @@
+package com.example.livoappofbooks.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.livoappofbooks.ui.theme.PrincipalColor
+
+@Composable
+fun ProgressBarSimple(
+    progress: Float, modifier: Modifier = Modifier
+) {
+    val percentage = (progress * 100).toInt()
+
+    Row(
+        modifier = modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(10.dp)
+                .clip(RoundedCornerShape(50))
+                .background(PrincipalColor.copy(alpha = 0.25f))
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(progress)
+                    .fillMaxHeight()
+                    .clip(RoundedCornerShape(50))
+                    .background(PrincipalColor)
+            )
+        }
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = "$percentage%", style = TextStyle(
+                color = PrincipalColor, fontSize = 12.sp, fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProgressBarSimple() {
+    ProgressBarSimple(progress = 0.30f)
+}
