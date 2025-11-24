6bcf07b6e61d75ff556f8726db8135a00dceee2a
RewelliOliveira <rewelli@alu.ufc.br>
2025-11-24 15:49:55 -0300

    feat(CustomIcons): Adição do icone de Close

--- app/src/main/java/com/example/livoappofbooks/ui/icons/CustomIcons.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/icons/CustomIcons.kt
@@ -122,6 +122,40 @@ val BuildingLibrary: ImageVector
         return _BuildingLibrary!!
     }
 
+val Close_small: ImageVector
+    get() {
+        if (_closeSmall != null) return _closeSmall!!
+
+        _closeSmall = ImageVector.Builder(
+            name = "Close_small",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black)
+            ) {
+                moveTo(19f, 6.41f)
+                lineTo(17.59f, 5f)
+                lineTo(12f, 10.59f)
+                lineTo(6.41f, 5f)
+                lineTo(5f, 6.41f)
+                lineTo(10.59f, 12f)
+                lineTo(5f, 17.59f)
+                lineTo(6.41f, 19f)
+                lineTo(12f, 13.41f)
+                lineTo(17.59f, 19f)
+                lineTo(19f, 17.59f)
+                lineTo(13.41f, 12f)
+                close()
+            }
+        }.build()
+
+        return _closeSmall!!
+    }
+
+private var _closeSmall: ImageVector? = null
 
 val BookOpen: ImageVector
     get() {
