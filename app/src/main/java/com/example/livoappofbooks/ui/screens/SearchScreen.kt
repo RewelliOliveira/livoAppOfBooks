a357c451773d2c9b931e3860adc53303105046c6
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-12-01 09:24:26 -0300

    feat: criação do arquivo da tela de busca

---
+++ app/src/main/java/com/example/livoappofbooks/ui/screens/SearchScreen.kt
@@ -0,0 +1,11 @@
+package com.example.livoappofbooks.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import com.example.livoappofbooks.ui.components.SearchBar
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen(){
+    Box(){
+    }
+}
