be6140bd2ff13453be5201d4dcd3075531f69fb6
RewelliOliveira <rewelli@alu.ufc.br>
2025-12-03 23:33:04 -0300

    feat: adição do retrofit e uso do token

---
+++ app/src/main/java/com/example/livoappofbooks/data/remote/RetrofitInstance.kt
@@ -0,0 +1,46 @@
+package com.example.livoappofbooks.data.remote
+
+import android.content.Context
+import com.example.livoappofbooks.data.remote.local.TokenManager
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitInstance {
+
+    private var api: AuthService? = null
+
+    fun getApi(context: Context): AuthService {
+        if (api == null) {
+            val tokenManager = TokenManager(context.applicationContext)
+
+            val authInterceptor = Interceptor { chain ->
+                val requestBuilder = chain.request().newBuilder()
+                tokenManager.getToken()?.let { token ->
+                    requestBuilder.addHeader("Authorization", "Bearer $token")
+                }
+                chain.proceed(requestBuilder.build())
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .build()
+
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8080/")
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(client)
+                .build()
+
+            api = retrofit.create(AuthService::class.java)
+        }
+        return api!!
+    }
+}
