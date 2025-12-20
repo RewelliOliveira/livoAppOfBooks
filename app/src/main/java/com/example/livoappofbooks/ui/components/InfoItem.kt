17d33b850ceb258ec04adb419751c6f3bb83632a
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-12-20 12:32:08 -0300

    refactor: ajuste geral para adaptabilidade

--- app/src/main/java/com/example/livoappofbooks/ui/components/InfoItem.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/InfoItem.kt
@@ -1,14 +1,11 @@
 package com.example.livoappofbooks.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,33 +18,25 @@ import com.example.livoappofbooks.ui.theme.*
 fun InfoItem(
     icon: ImageVector,
     text: String,
-    width: Dp? = null,
-    height: Dp? = null,
+    iconSize: Dp = 14.dp,
+    textStyle: androidx.compose.ui.text.TextStyle = AppTypography.bodySmall
 ) {
     Row(
-        modifier = Modifier
-            .then(
-                if (width != null) Modifier.width(width) else Modifier
-            )
-            .then(
-                if (height != null) Modifier.height(height) else Modifier
-            )
-            .wrapContentWidth()
-            .wrapContentHeight(),
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
         Icon(
             imageVector = icon,
             contentDescription = null,
             tint = onBackground ,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier.size(iconSize)
         )
 
         Spacer(Modifier.width(6.dp))
 
         Text(
             text = text,
-            style = AppTypography.bodyMedium,
+            style = textStyle,
             color = onBackground
         )
     }
