b1d7f765bb4198f28c0f279cfb7a66bedfc12fed
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-12-20 12:32:51 -0300

    style: ajuste nas cores

--- app/src/main/java/com/example/livoappofbooks/ui/components/SearchBar.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/SearchBar.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.livoappofbooks.ui.icons.MagnifyingGlass
 import com.example.livoappofbooks.ui.theme.*
 
 @Composable
@@ -37,7 +38,7 @@ fun SearchBar(
             .background(background)
             .border(
                 width = 2.dp,
-                color = primary,
+                color = outline,
                 shape = RoundedCornerShape(20.dp)
             )
     ) {
@@ -70,7 +71,7 @@ fun SearchBar(
                                     fontWeight = FontWeight.Normal,
                                     fontSize = 10.sp,
                                     lineHeight = 10.sp,
-                                    color = primary
+                                    color = Gray
                                 )
                             )
                         }
@@ -82,9 +83,9 @@ fun SearchBar(
             Spacer(modifier = Modifier.width(12.dp))
 
             Icon(
-                imageVector = Icons.Default.Search,
+                imageVector = MagnifyingGlass,
                 contentDescription = "Pesquisar",
-                tint = primary,
+                tint = outline,
                 modifier = Modifier.size(28.dp).clickable { onSearch() }
             )
         }
