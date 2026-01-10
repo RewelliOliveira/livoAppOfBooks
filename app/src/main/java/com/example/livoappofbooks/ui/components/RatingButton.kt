8a3b0c2e74b21768577e86829eec67201eff855a
Julian Cardoso <juliancesarcardoso@gmail.com>
2026-01-10 17:21:44 -0300

    style: ajuste para tema escuro

--- app/src/main/java/com/example/livoappofbooks/ui/components/RatingButton.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/RatingButton.kt
@@ -1,19 +1,15 @@
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.livoappofbooks.ui.theme.*
 import com.example.livoappofbooks.ui.icons.*
@@ -27,12 +23,12 @@ fun RatingButton(
         onClick = onClick,
         shape = RoundedCornerShape(50),
         colors = ButtonDefaults.outlinedButtonColors(
-            contentColor = primary,
+            contentColor = outline,
             disabledContentColor = primary.copy(alpha = 0.5f)
         ),
         border = BorderStroke(
             1.dp,
-            primary
+            outline
         ),
         modifier = modifier
             .defaultMinSize(minHeight = 40.dp)
@@ -41,7 +37,7 @@ fun RatingButton(
         Icon(
             imageVector = Star,
             contentDescription = "Estrela",
-            tint = primary,
+            tint = outline,
             modifier = Modifier.size(18.dp)
         )
 
@@ -50,17 +46,6 @@ fun RatingButton(
         Text(
             text = "Avaliar livro",
             style = AppTypography.titleSmall.copy(color = primary)
-        )
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun SecondaryButtonPreview() {
-    Box(Modifier.padding(16.dp)) {
-       RatingButton(
-            onClick = {},
         )
     }
 }
