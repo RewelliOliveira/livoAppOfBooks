1b295f95010270d05b7633e65bfae33b38366283
Julian Cardoso <juliancesarcardoso@gmail.com>
2026-01-10 17:22:13 -0300

    style: ajuste para tema escuro

--- app/src/main/java/com/example/livoappofbooks/ui/components/ShelfItem.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/ShelfItem.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.livoappofbooks.ui.icons.Arrow_forward_ios_new
 import com.example.livoappofbooks.ui.screens.Prateleira
-import com.example.livoappofbooks.ui.theme.primary
+import com.example.livoappofbooks.ui.theme.outline
 import com.example.livoappofbooks.ui.theme.tertiary
 import androidx.compose.ui.res.painterResource
 import com.example.livoappofbooks.R
@@ -114,7 +114,7 @@ fun ShelfItem(
                 imageVector = Arrow_forward_ios_new,
                 contentDescription = "Abrir",
                 modifier = Modifier.size(28.dp),
-                colorFilter = ColorFilter.tint(primary)
+                colorFilter = ColorFilter.tint(outline)
             )
         }
     }
