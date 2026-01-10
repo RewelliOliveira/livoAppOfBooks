27de1d5c35e6f5996b84cd2121fb0fd610208d68
Julian Cardoso <juliancesarcardoso@gmail.com>
2026-01-10 16:05:38 -0300

    cleanup: retirando preview

--- app/src/main/java/com/example/livoappofbooks/ui/components/Book.kt
+++ app/src/main/java/com/example/livoappofbooks/ui/components/Book.kt
@@ -143,15 +143,3 @@ fun Book(
         }
     }
 }
-
-@Preview
-@Composable
-fun BookPreview() {
-    Book(
-        status = BookStatus.QUERO_LER,
-        progress = 0,
-        evaluate = 0,
-        imageUrl = "",
-        onClick = {}
-    )
-}
