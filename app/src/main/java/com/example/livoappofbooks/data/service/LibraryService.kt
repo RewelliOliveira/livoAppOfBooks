3729737905508614e6d9d71e8e719c0b3b7e66ab
RewelliOliveira <rewelli@alu.ufc.br>
2026-01-11 02:26:00 -0300

    config: post de rating

--- app/src/main/java/com/example/livoappofbooks/data/service/LibraryService.kt
+++ app/src/main/java/com/example/livoappofbooks/data/service/LibraryService.kt
@@ -3,15 +3,23 @@ package com.example.livoappofbooks.data.service
 import com.example.livoappofbooks.data.model.Book
 import com.example.livoappofbooks.data.model.LibraryBookReponse
 import com.example.livoappofbooks.data.model.UserProfile
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
 data class StatusUpdateRequest(
     val status: String
+)
+
+@JsonClass(generateAdapter = true)
+data class RatingRequest(
+    @Json(name = "rating") val rating: Int
 )
 
 interface LibraryService {
@@ -33,5 +41,12 @@ interface LibraryService {
     @DELETE("/library/{book_id}")
     suspend fun removeBookFromLibrary(
         @Path("book_id") userBookId: String
+    ): Response<Unit>
+
+    // NOVO ENDPOINT DE AVALIAÇÃO
+    @POST("/books/{book_id}/rating")
+    suspend fun registerBookRating(
+        @Path("book_id") bookId: String,
+        @Body request: RatingRequest
     ): Response<Unit>
 }
