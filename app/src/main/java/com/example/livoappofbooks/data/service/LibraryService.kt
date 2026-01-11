58f1dff1144620e5fcc488bf258ada6551061856
RewelliOliveira <rewelli@alu.ufc.br>
2026-01-11 00:08:20 -0300

    feat: put de status

--- app/src/main/java/com/example/livoappofbooks/data/service/LibraryService.kt
+++ app/src/main/java/com/example/livoappofbooks/data/service/LibraryService.kt
@@ -4,16 +4,24 @@ import com.example.livoappofbooks.data.model.Book
 import com.example.livoappofbooks.data.model.LibraryBookReponse
 import com.example.livoappofbooks.data.model.UserProfile
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
+
+data class StatusUpdateRequest(
+    val status: String
+)
 
 interface LibraryService {
     @GET("/library")
     suspend fun getUserBooks(): Response<List<LibraryBookReponse>>
-
     @GET("/books/{book_id}")
-    suspend fun getBookById(@Path("book_id") bookId: String): Response<Book>
-
-    @GET("user/profile")
+    suspend fun getBookById(@Path("book_id") bookId: String): Response<Book>@GET("user/profile")
     suspend fun getUserProfile(): UserProfile
+    @PUT("/library/{book_id}")
+    suspend fun updateBookStatus(
+        @Path("book_id") bookId: String,
+        @Body request: StatusUpdateRequest
+    ): Response<Unit>
 }
