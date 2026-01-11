8c2552c0577bb8364eb3487a7e62324cc1c6c2ad
RewelliOliveira <rewelli@alu.ufc.br>
2026-01-11 00:55:18 -0300

    config: url de delete

--- app/src/main/java/com/example/livoappofbooks/data/service/LibraryService.kt
+++ app/src/main/java/com/example/livoappofbooks/data/service/LibraryService.kt
@@ -5,6 +5,7 @@ import com.example.livoappofbooks.data.model.LibraryBookReponse
 import com.example.livoappofbooks.data.model.UserProfile
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -16,12 +17,21 @@ data class StatusUpdateRequest(
 interface LibraryService {
     @GET("/library")
     suspend fun getUserBooks(): Response<List<LibraryBookReponse>>
+
     @GET("/books/{book_id}")
-    suspend fun getBookById(@Path("book_id") bookId: String): Response<Book>@GET("user/profile")
+    suspend fun getBookById(@Path("book_id") bookId: String): Response<Book>
+
+    @GET("user/profile")
     suspend fun getUserProfile(): UserProfile
+
     @PUT("/library/{book_id}")
     suspend fun updateBookStatus(
         @Path("book_id") bookId: String,
         @Body request: StatusUpdateRequest
+    ): Response<Unit>
+
+    @DELETE("/library/{book_id}")
+    suspend fun removeBookFromLibrary(
+        @Path("book_id") userBookId: String
     ): Response<Unit>
 }
